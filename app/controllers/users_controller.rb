4b05c06a7b121ad0893d80dd490c60e4e5a517a1
splashkida <seira.kida0406@gmail.com>
2021-02-14 22:08:02 +0900

    ビュー整形

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    redirect_to root_path unless current_user.id == @item.user_id
     @nickname = current_user.nickname
     @memories = current_user.memories
   end
