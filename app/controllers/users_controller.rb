68360238e1131efdd0a418d88918e143e81b33d5
splashkida <seira.kida0406@gmail.com>
2021-02-14 21:49:26 +0900

    マイページの修正
    ログイン中のユーザーしかマイページへ遷移できない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    redirect_to root_path unless current_user.id == @item.user_id
     @nickname = current_user.nickname
     @memories = current_user.memories
   end
